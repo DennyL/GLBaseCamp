b0f2d51a4be0072c5e90ce39bee372e022c69eb9
DennyL <denis.lozinskij@gmail.com>
2019-06-22 13:53:50 +0300

    improvement

--- Homework_3/src/Tests/test_delete_note.py
+++ Homework_3/src/Tests/test_delete_note.py
@@ -8,12 +8,12 @@ page = HomePage()
 @pytest.fixture()
 def initial_actions():
     page.openApp()
+    page.createNote()
     yield
     page.closeApp()
 
 
 @allure.title('GL-244:F-15.4: Deleting notes ability verification')
 def test_delete(initial_actions):
-    page.createNote()
     page.deleteNote()
     assert page.savedNotesBlockText() == 'No note here.'
